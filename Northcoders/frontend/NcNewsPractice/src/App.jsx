cd330735f8ca667435d27eaa79e57a5e6ea4cd9e
Nacershucks <zack.sawyerr@gmail.com>
2024-03-05 21:57:04 +0000

    fixing file system

---
+++ Northcoders/frontend/NcNewsPractice/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    <body>
+      sdasda
+    </body>
+    </>
+      
+
+  )
+}
+
+export default App
